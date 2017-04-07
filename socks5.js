01db75173bc34c086334cf69f81205dc62c5234e
andersoncontreira-rentcars <anderson.contreira@rentcars.com>
2017-04-07 10:56:27 -0300

    Ajustes de navegacao entre links e preparacao para o refactor

--- socks5.js
+++ socks5.js
@@ -1,24 +1,34 @@
 var cmd = require('node-cmd');
 var express = require('express');
 var bodyParser = require('body-parser');
+var program = require('commander');
+var utf8 = require('utf8');
 var favicon = require('serve-favicon');
 var path = require('path');
 var http = require('http');
 var request = require('request');
+var url = require('url');
 var Agent = require('socks5-http-client/lib/Agent');
 var HttpsAgent = require('socks5-https-client/lib/Agent');
 var DefaultRequest = require('./request/DefaultRequest');
 
+
 /**
  * Default
  */
-var consoleFlag = 'socks5-> ';
+var consoleFlag = 'tunnel-> ';
 var socks5Host = '127.0.0.1';
 var socks5Port = 2222;
 var httpPort = 3456;
 var useSocksFive = true;
+/**
+ * Ultima requisição
+ * Serve para permitir a navegação
+ * @type {Object}
+ */
+var lastRequest = null;
 
-var program = require('commander');
+
 
 program
     .version('0.0.1')
@@ -50,6 +60,32 @@ app.use(bodyParser.urlencoded({extended: false}));
 //Read XML request as Text
 app.use(bodyParser.text({type: ['*/xml', '+xml']}));
 
+function parseUrlRequested(urlRequested) {
+    return url.parse(urlRequested);
+}
+
+
+function encodeRequestObject(requestObject) {
+
+
+    var urlRequested = '';
+    urlRequested += requestObject['protocol']+'//';
+    urlRequested += requestObject['host']+'/';
+    if (requestObject['pathname'] !== null) {
+        urlRequested += requestObject['pathname'];
+    }
+    if (requestObject['query'] !== null) {
+        urlRequested += '?'+requestObject['query'];
+    }
+    if (requestObject['hash'] !== null) {
+        urlRequested += requestObject['hash'];
+    }
+
+
+
+
+    return urlRequested;
+}
 
 function executeHttpRequest(defaultRequest, callback) {
 
@@ -58,7 +94,43 @@ function executeHttpRequest(defaultRequest, callback) {
     if (defaultRequest.getUrl().match('https')) {
         agent = HttpsAgent;
     }
-    //process.exit(0);
+
+    if (lastRequest != null) {
+        var requestObject = url.parse(defaultRequest.getUrl());
+
+        /**
+         * Se estiver nulo as propriedades básicas da requisição, significa que é uma
+         * requisição no mesmo servidor, por exemplo o caso do google:
+         * Requisição Inicial:
+         * http://www.google.com/
+         * Sub-requisições:
+         * textinputassistant/tia.png
+         * client_204?&atyp=i&biw=1920&bih=463&ei=RvnkWJzgK4KfwASJ-Y-gAQ
+         * ....
+         */
+        if (requestObject.hasOwnProperty('protocol') && requestObject['protocol'] == null
+            && requestObject.hasOwnProperty('host') && requestObject['host'] == null) {
+
+            var notCopy = ['hash','query','search'];
+            /**
+             * Sobrescreve os parametros nulos
+             */
+            for (var p in requestObject) {
+                if( requestObject[p] == null && notCopy.indexOf(p) === -1) {
+                    console.log(p);
+                    requestObject[p] = lastRequest[p];
+                }
+            }
+        }
+        /**
+         * converte o Objeto para a url
+         */
+        //console.log(requestObject, lastRequest);
+        var requestedUrl = encodeRequestObject(requestObject);
+        defaultRequest.setUrl(requestedUrl);
+
+    }
+
 
     var options = {
         method: defaultRequest.getMethod(),
@@ -78,32 +150,40 @@ function executeHttpRequest(defaultRequest, callback) {
         options['headers'] = defaultRequest.getHeaders();
     }
 
+    if (options.hasOwnProperty('headers') && options['headers'].hasOwnProperty('host')) {
+        /**
+         * Se for localhost remove porque a maioria das locadoras dão erro de resposta http
+         */
+        if (options['headers']['host'].match('localhost')) {
+            delete options['headers']['host'];
+        }
 
+    }
     console.log(consoleFlag + ' options:', options);
 
 
     request(options, function (error, res) {
-        //console.log('aq');
-        //console.log(res.headers);
-        //console.log('res',res);
-        //var data = "";
-
-        //if(res != undefined && res.hasOwnProperty('body')) {
-        //    data = res.body;
-        //}
 
         console.log(consoleFlag + ' ----------------------------------- ');
         console.log(consoleFlag + ' Response ');
         console.log(consoleFlag + ' ----------------------------------- ');
-        //console.log(consoleFlag + ' Response-length: ' + data.length);
-        //console.log(consoleFlag + ' Response-length: ' + res.length);
 
         if (error) {
-            console.log(consoleFlag + ' Response-status: Error!');
+            console.log(consoleFlag + ' Response-Status: Error!');
             console.log(consoleFlag + ' ' + error.message);
             callback(error, res);
         } else {
-            console.log(consoleFlag + ' Response-status: Success!');
+
+            var requestObject = parseUrlRequested(defaultRequest.getUrl());
+            /**
+             * Sobrescreve o objeto da ultima requisição
+             */
+            if (requestObject.hasOwnProperty('protocol') && requestObject['protocol'] != null
+                && requestObject.hasOwnProperty('host') && requestObject['host'] != null) {
+                lastRequest = requestObject;
+            }
+            console.log(consoleFlag + ' Response-Status: Success!');
+
             callback(null, res);
         }
     });
@@ -128,8 +208,14 @@ app.use(function (req, res, next) {
         delete req.headers['accept-encoding'];
     }
 
+    /**
+     * Testes
+     * @type {string}
+     */
+    //req.headers['content-type'] = 'text/html; charset=UTF-8';
+
     var method = req.method;
-    var url = req.url.slice(1, req.length);
+    var requestedUrl = req.url.slice(1, req.length);
     var headers = req.headers;
     var body = req.body;
 
@@ -137,35 +223,44 @@ app.use(function (req, res, next) {
     console.log(consoleFlag + ' Request');
     console.log(consoleFlag + ' ----------------------------------- ');
     console.log(consoleFlag + ' method:', method);
-    console.log(consoleFlag + ' url:', url);
+    console.log(consoleFlag + ' url:', requestedUrl);
     console.log(consoleFlag + ' headers:', headers);
     console.log(consoleFlag + ' body:', body);
 
     var defaultRequest = new DefaultRequest();
     defaultRequest.setMethod(method);
-    defaultRequest.setUrl(url);
+    defaultRequest.setUrl(requestedUrl);
     defaultRequest.setHeaders(headers);
     defaultRequest.setBody(body);
 
     executeHttpRequest(defaultRequest, function (error, result) {
         if (error) {
             res.send(error.message);
+
         } else {
 
             for (var header in result.headers) {
                 res.removeHeader(header);
 
-                //console.log("cr:->",header,result.headers[header]);
-
                 res.setHeader(header, result.headers[header]);
 
-                //console.log("gt:->",header,res.getHeader(header));
             }
+
+            /**
+             * Testes
+             */
+            //res.setHeader('content-type','text/html; charset=UTF-8');
+
+            /**
+             * Tratar o tipo de retorno quando imagem etc
+             */
+            console.log('result.headers',result.headers);
             res.send(result.body);
+            //res.send(utf8.decode(result.body));
         }
     });
 });
 
 app.listen(httpPort, function () {
-    console.log(consoleFlag + ' server up in port:' + httpPort);
+    console.log(consoleFlag + ' Server up in port:' + httpPort);
 });
