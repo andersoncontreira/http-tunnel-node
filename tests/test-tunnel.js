f7f8f6572fd994c285b0053e5a54e1e4f6d62a88
Anderson Contreira <anderson.contreira@rentcars.com>
2019-03-13 13:52:09 -0300

    inclusao de comandos do codecov

---
+++ tests/test-tunnel.js
@@ -0,0 +1,12 @@
+const chai = require('chai')
+const assert = chai.assert
+
+const tunnel = require(process.cwd()+'/modules/Tunnel')
+
+describe('Test.Tunnel', function () {
+
+  it('should execute OK', function (done) {
+    console.log('OK')
+    done()
+  })
+})
